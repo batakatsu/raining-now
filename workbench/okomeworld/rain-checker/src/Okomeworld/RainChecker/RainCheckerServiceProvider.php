ffdf5ed40346d4ddb2d672f4446a0f73c10c3e00
okomeworld <rwmh0209@gmail.com>
2014-09-09 02:46:12 +0900

    パッケージのひな形を作成
    雨降ってるかチェックするクラス"RainChecker"を作る。
    laravelのworkbench生成を使用

---
+++ workbench/okomeworld/rain-checker/src/Okomeworld/RainChecker/RainCheckerServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Okomeworld\RainChecker;
+
+use Illuminate\Support\ServiceProvider;
+
+class RainCheckerServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
