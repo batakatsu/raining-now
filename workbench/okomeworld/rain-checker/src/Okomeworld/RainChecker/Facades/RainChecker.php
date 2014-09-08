d0f5c3e76a8525ed31165b5c7c7bad24a64d0c62
okomeworld <rwmh0209@gmail.com>
2014-09-09 03:20:55 +0900

    Facades設定の追加
    App::make('rain_checker')->method()を
    RainChecker::method()で呼び出せるようにする設定らしい

---
+++ workbench/okomeworld/rain-checker/src/Okomeworld/RainChecker/Facades/RainChecker.php
@@ -0,0 +1,8 @@
+<?php namespace Okomeworld\RainChecker\Facades;
+
+class RainChecker extends\Illuminate\Support\Facades\Facade {
+
+	protected static function getFacadeAccessor() {
+		return 'rain_checker';
+	}
+}
