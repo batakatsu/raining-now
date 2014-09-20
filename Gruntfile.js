120e416d88d5115b82795edf6a995f798fab4a92
kawabataryo <ryo.wizard@gmail.com>
2014-09-20 17:15:18 +0900

    Gruntタスクを作成

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,48 @@
 module.exports = function(grunt) {
-	grunt.registerTask('heroku:development', 'Grunt Test', function() {
-		grunt.log.write('Logging for Grunt Test').ok();
+
+	grunt.initConfig({
+		sass: {
+			options: {
+				style: 'compressed',
+				sourcemap: true,
+				noCache: true
+			},
+			styles: {
+				src: 'scss/style.scss',
+				dest: 'public/stylesheets/style.css'
+			}
+		},
+		uglify: {
+			js: {
+				src: "public/js/app.js",
+				dest: "public/js/app.min.js"
+			}
+		},
+		concat : {
+			js : {
+				src : [//対象のJSファイルを記述
+				'js/sample01.js',
+				'js/sample02.js'
+				],
+				dest : 'public/js/app.js'
+			}
+		},
+		watch: {
+			sass: {
+				files: ["scss/*.scss"],
+				tasks: ['sass']
+			},
+			js: {
+				files: ["public/js/*.js"],
+				tasks: ['concat','uglify']
+			}
+		}
+
 	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
 };
