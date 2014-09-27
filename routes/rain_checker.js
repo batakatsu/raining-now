6e37672f7a5dd1cdeb90173cabdb0f0d4e197557
okomeworld <rwmh0209@gmail.com>
2014-09-27 18:45:11 +0900

    /rain_checkerにlatとlngを渡してやると直近の降水量が取得されるように実装

--- routes/rain_checker.js
+++ routes/rain_checker.js
@@ -1,8 +1,28 @@
-var express = require('express');
-var router = express.Router();
+var express      = require('express');
+var validator    = require('validator');
+var router       = express.Router();
 
-router.get('/', function(req, res) {
-	res.send(req.query);
+var RainChecker = require('rain_checker');
+
+router.get('/', function(req, res, next) {
+	var lat = validator.toFloat(req.query.lat);
+	var lng = validator.toFloat(req.query.lng);
+
+	if (!lat || !lng) {
+		var error = new Error('Undefined lat or lng param');
+		error.status = 500;
+		next(error);
+	}
+
+	var rain_checker = new RainChecker(lat, lng);
+
+	if (!rain_checker.is_request_success()) {
+		var error = new Error('Failed rquest to API');
+		error.status = 500;
+		next(error);
+	}
+
+	res.send(rain_checker.get_recent_weather());
 });
 
 module.exports = router;
