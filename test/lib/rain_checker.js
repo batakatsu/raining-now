28678a0c113379c0a8273300b21983a1531267ea
okomeworld <rwmh0209@gmail.com>
2014-09-27 16:49:09 +0900

    get_weathersを実装

--- test/lib/rain_checker.js
+++ test/lib/rain_checker.js
@@ -5,12 +5,10 @@ date = new Date();
 
 var lat = 35.011636;
 var lng = 135.768029;
-var opt = {
-	date: date.toFormat('YYYYMMDDHH24MI')
-};
+var opt = {};
 
 var rain_checker = new RainChecker(lat, lng, opt);
 
 console.log(opt);
 console.log(rain_checker.is_request_success());
-console.log(require('util').inspect(rain_checker.data.Feature[0],false,null));
+console.log(rain_checker.get_weathres());
