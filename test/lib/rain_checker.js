4fcdfb5bac31436af6a4a11af97b41ffbd9ce569
okomeworld <rwmh0209@gmail.com>
2014-09-27 14:31:12 +0900

    is_request_successを実装した

--- test/lib/rain_checker.js
+++ test/lib/rain_checker.js
@@ -1,7 +1,16 @@
+require('date-utils');
 var RainChecker = require('rain_checker');
+
+date = new Date();
 
 var lat = 35.011636;
 var lng = 135.768029;
-var rain_checker = new RainChecker(lat, lng);
+var opt = {
+	date: date.toFormat('YYYYMMDDHH24MI')
+};
 
-console.log(rain_checker);
+var rain_checker = new RainChecker(lat, lng, opt);
+
+console.log(opt);
+console.log(rain_checker.is_request_success());
+console.log(require('util').inspect(rain_checker.data.Feature[0],false,null));
